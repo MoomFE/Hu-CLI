9311ae3aed125777deca95c51a61db6cc057774e
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-14 17:58:11 +0800

    完成了最基本的 watch 指令功能

---
+++ scripts/init/watch.js
@@ -0,0 +1,15 @@
+const rollup = require('rollup');
+
+module.exports = async () => {
+  const configs = await require('./basic/index.js')();
+  const watcher = rollup.watch(
+    configs.map( config => {
+      return Object.assign( config.input, {
+        output: config.output,
+        watch: {
+          chokidar: true
+        }
+      });
+    })
+  );
+}
