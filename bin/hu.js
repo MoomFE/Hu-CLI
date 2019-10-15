e66f2350a4e6d6a8bda8fefc5cbfb88be4b1f20d
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-15 14:58:40 +0800

    添加环境变量 HU_RUNNING_COMMAND, 标识当前 CLI 正在执行的指令

--- bin/hu.js
+++ bin/hu.js
@@ -11,12 +11,16 @@ program.version(
 program
   .command( 'watch' )
   .action(() => {
+    process.env.HU_RUNNING_COMMAND = 'watch';
+
     require('../scripts/init/watch.js')();
   });
 
 program
   .command( 'build' )
   .action(() => {
+    process.env.HU_RUNNING_COMMAND = 'build';
+
     require('../scripts/init/build.js')();
   });
 
