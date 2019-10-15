c3545e498cfc976c40045ff59f8953a85bdd7406
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-15 16:33:48 +0800

    细节修改

--- scripts/init/watch.js
+++ scripts/init/watch.js
@@ -5,7 +5,7 @@ module.exports = async () => {
   const configs = await require('./basic/index.js')();
   const watcher = rollup.watch(
     configs.map( config => {
-      return Object.assign( config.input, {
+      return Object.assign( {}, config.input, {
         output: config.output,
         watch: {
           chokidar: true
