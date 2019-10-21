e5e436d143c5f6460870207b8d5e1b5660404cf4
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-21 14:08:47 +0800

    使用 watch 指令时, 配置文件发生更改会自动重新启动打包程序

--- scripts/init/watch.js
+++ scripts/init/watch.js
@@ -1,9 +1,16 @@
+const { resolve } = require('path');
 const rollup = require('rollup');
+const chokidar = require('chokidar');
+const print = require('../utils/print.js');
+
+
+let rollupWatcher = null;
 
 
 module.exports = async () => {
   const configs = await require('./basic/index.js')();
-  const watcher = rollup.watch(
+
+  rollupWatcher = rollup.watch(
     configs.map( config => {
       return Object.assign( {}, config.input, {
         output: config.output,
@@ -13,4 +20,23 @@ module.exports = async () => {
       });
     })
   );
+}
+
+
+{
+  const root = process.cwd();
+  const configFile = resolve( root, 'hu.config.js' );
+
+  chokidar.watch( configFile ).on( 'change', ( path, stats ) => {
+    if( rollupWatcher ){
+      console.log('\n');
+      print.start('配置文件发生更改, 正在重启打包程序 ...');
+      print.end();
+
+      rollupWatcher.close();
+      rollupWatcher = null;
+
+      module.exports();
+    }
+  });
 }
