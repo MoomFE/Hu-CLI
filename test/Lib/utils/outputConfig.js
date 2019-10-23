b5c82dc00cbdb4865ac0b2390328fe3d72908e48
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-23 14:09:15 +0800

    美化下配置文件的输出
    虽然并没啥卵用

--- test/Lib/utils/outputConfig.js
+++ test/Lib/utils/outputConfig.js
@@ -4,10 +4,10 @@ const { root } = require('../const');
 
 
 module.exports = ( config ) => {
-  const toString = JSON.stringify( config );
+  const toString = JSON.stringify( config, null, 2 );
   const data = `module.exports = ${
     toString
-  }`;
+  };`;
 
 
   return outputFile(
