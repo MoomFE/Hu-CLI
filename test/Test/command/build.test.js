03a8a7cefa317ad9a3af92b966b1993c3730ec56
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-23 11:48:36 +0800

    完善单元测试

--- test/Test/command/build.test.js
+++ test/Test/command/build.test.js
@@ -6,21 +6,33 @@ describe( 'command.build', function(){
 
   it( '在使用 build 指令时, 在生成打包配置时会自动加载 terser 用于代码压缩', () => {
     process.env.HU_RUNNING_COMMAND = 'build';
+    {
+      const rollupConfig = compilerRollupConfigs()[0];
 
-    const rollupConfig = compilerRollupConfigs()[0];
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'terser' })
+      ).is.not.undefined;
+    }
 
-    expect(
-      rollupConfig.input.plugins.$find({ name: 'terser' })
-    ).is.not.undefined;
-
-    // 翻转测试
+    // 使用 watch 指令
     process.env.HU_RUNNING_COMMAND = 'watch';
+    {
+      const rollupConfig2 = compilerRollupConfigs()[0];
 
-    const rollupConfig2 = compilerRollupConfigs()[0];
+      expect(
+        rollupConfig2.input.plugins.$find({ name: 'terser' })
+      ).is.undefined;
+    }
 
-    expect(
-      rollupConfig2.input.plugins.$find({ name: 'terser' })
-    ).is.undefined;
+    // 使用其他指令
+    process.env.HU_RUNNING_COMMAND = '';
+    {
+      const rollupConfig3 = compilerRollupConfigs()[0];
+
+      expect(
+        rollupConfig3.input.plugins.$find({ name: 'terser' })
+      ).is.undefined;
+    }
   });
 
 });
