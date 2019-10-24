e0d44670cadde549a38311d0a22a6ed9753720a1
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-24 10:54:17 +0800

    使 replace 支持传入 Map 类型的选项以支持自定义正则替换

--- scripts/plugins/replace.js
+++ scripts/plugins/replace.js
@@ -3,16 +3,38 @@ require('@moomfe/zenjs');
 
 module.exports = ( config ) => {
   const replace = config.replace;
+  const replaceMap = new Map;
 
+  if( !replace ){
+    return;
+  }
 
-  if( replace && Object.$isPlainObject( replace ) && Object.$isEmptyObject( replace ) === false ){
-    const replaceMap = new Map;
+  // 正常传参
+  if( Object.$isPlainObject( replace ) ){
+    // 空对象
+    if( Object.$isEmptyObject( replace ) ){
+      return;
+    }
 
     Object.entries( replace ).$each(([ key, value ]) => {
-      key = RegExp.$parse( key, 'g' );
-      replaceMap.set( key, value );
+      const regKey = RegExp.$parse( key, 'g' );
+      replaceMap.set( regKey, value );
     });
+  }
+  // Map 类型传参支持
+  else if( replace instanceof Map ){
+    replace.forEach(( value, key ) => {
+      if( ZenJS.isString( key ) || ZenJS.isRegExp( key ) ){
+        replaceMap.set( key, value );
+      }
+    });
+  }
+  // 不支持的值
+  else{
+    return;
+  }
 
+  if( replaceMap.size ){
     return {
       name: 'replace',
       transform( code, id ){
