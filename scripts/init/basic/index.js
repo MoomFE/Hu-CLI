7f12802f9b5811a3715a537ab91e2bcd322ac750
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-28 10:36:01 +0800

    优化错误提示的输出

--- scripts/init/basic/index.js
+++ scripts/init/basic/index.js
@@ -11,33 +11,36 @@ const compilerRollupConfigs = require('./util/compilerRollupConfigs.js');
 module.exports = async () => {
   const originConfigs = await getConfigFile();
   const configs = compilerConfigs( originConfigs );
+  const errors = new Set;
 
   // 规避一些小问题
   for( const config of configs ){
     // 未定义 format 选项
     if( !config.format || [ 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' ].includes( config.format ) ){
-      print.start();
-      print.end(`${ bgBlackBright(' format ') } : 选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个 !`);
-      process.exit( 0 );
+      errors.add(`${ bgBlackBright(' format ') } : 选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个 !`);
     }
     // 选项 pluginOptions 并非是一个纯粹的对象
     if( Object.$isPlainObject( config.pluginOptions ) === false ){
-      print.start();
-      print.end(`${ bgBlackBright(' pluginOptions ') } : 选项必须为一个纯粹的对象, 请检查您的配置文件 !`);
-      process.exit( 0 );
+      errors.add(`${ bgBlackBright(' pluginOptions ') } : 选项必须为一个纯粹的对象, 请检查您的配置文件 !`);
     }
     // 选项 plugins 并非是一个函数
     if( typeof config.plugins !== 'function' ){
-      print.start();
-      print.end(`${ bgBlackBright(' plugins ') } : 选项必须为一个函数并且函数返回 plugins 数组, 请检查您的配置文件 !`);
-      process.exit( 0 );
+      errors.add(`${ bgBlackBright(' plugins ') } : 选项必须为一个函数并且函数返回 plugins 数组, 请检查您的配置文件 !`);
     }
     // 未找到入口文件
     if( await pathExists( config.input ) === false ){
-      print.start();
-      print.end(`未找到需要打包的入口文件 ( ${ yellow( config.input ) } ), 请确认后重试 !`);
-      process.exit( 0 );
+      errors.add(`未找到需要打包的入口文件 ( ${ yellow( config.input ) } ), 请确认后重试 !`);
     }
+  }
+
+  // 统一输出错误提示
+  if( errors.size ){
+    print.start();
+    errors.forEach( error => {
+      print.log( error );
+    });
+    print.end();
+    process.exit( 0 );
   }
 
   return compilerRollupConfigs( configs );
