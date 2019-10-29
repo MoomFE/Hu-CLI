4e7f5b1eb20e0960029a610b12109a1620c09a7a
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-10-29 09:35:55 +0800

    优化测试代码

---
+++ test/Lib/utils/proxyProcessExit.js
@@ -0,0 +1,19 @@
+module.exports = async ( fn ) => {
+  const processExit = process.exit;
+  let isExit = false;
+  
+  // 劫持进程退出
+  process.exit = () => {
+    isExit = true;
+  }
+
+  let result = fn();
+  if( result instanceof Promise ){
+    await result;
+  }
+
+  // 还原劫持进程退出
+  process.exit = processExit;
+
+  return isExit;
+}
--- test/Test/config/config.test.js
+++ test/Test/config/config.test.js
@@ -1,23 +1,10 @@
 const compilerRollupConfigs = require('../../Lib/compilerRollupConfigs');
 const proxyLog = require('../../Lib/utils/proxyLog');
+const proxyProcessExit = require('../../Lib/utils/proxyProcessExit');
 
 
 describe( 'config', function(){
   this.timeout( Infinity );
-
-  const processExit = {
-    fn: process.exit,
-    isExit: false,
-    start(){
-      this.isExit = false;
-      process.exit = () => {
-        this.isExit = true;
-      }
-    },
-    end(){
-      process.exit = this.fn;
-    }
-  };
 
   it( '未使用 pipe 选项进行多项目打包时, 当前配置表会视为一个有效的配置表', () => {
     expect(
@@ -37,78 +24,73 @@ describe( 'config', function(){
   });
 
   it( '对配置进行解析时, 若配置合法, 打包程序会正确执行', async () => {
-    processExit.start();
-    expect( processExit.isExit ).is.false;
+    const isExit = await proxyProcessExit( async () => {
+      const stdout = await proxyLog( async () => {
+        await compilerRollupConfigs( null, true );
+      });
 
-    const stdout = await proxyLog( async () => {
-      await compilerRollupConfigs( null, true );
+      expect( stdout ).is.equals('');
     });
 
-    expect( stdout ).is.equals('');
-    expect( processExit.isExit ).is.false;
-    processExit.end();
+    expect( isExit ).is.false;
   });
 
   it( '对配置进行解析时, 若配置不合法, 将会退出打包程序 ( format )', async () => {
     // 1
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: ''
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: ''
-        }, true);
+        expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
       });
 
-      expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
     // 2
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'xxx'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'xxx'
-        }, true);
+        expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
       });
 
-      expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
     // 3
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 0
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 0
-        }, true);
+        expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
       });
 
-      expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
     // 4
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: true
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: true
-        }, true);
+        expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
       });
 
-      expect( stdout ).is.includes(`选项必须为 'amd', 'cjs', 'system', 'esm', 'iife', 'umd' 中的一个`);
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
 
     // -------------------------------------------
@@ -117,110 +99,103 @@ describe( 'config', function(){
 
     // 1
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'amd'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'amd'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
     // 2
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'cjs'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'cjs'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
     // 3
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'system'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'system'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
     // 4
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'esm'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'esm'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
     // 5
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'iife'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'iife'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
     // 6
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'umd'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          format: 'umd'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
   });
 
   it( '对配置进行解析时, 若配置不合法, 将会退出打包程序 ( pluginOptions )', async () => {
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            pluginOptions: []
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          pluginOptions: []
-        }, true);
+        expect( stdout ).is.includes('选项必须为一个纯粹的对象, 请检查您的配置文件');
       });
 
-      expect( stdout ).is.includes('选项必须为一个纯粹的对象, 请检查您的配置文件');
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
 
     // -------------------------------------------
@@ -228,35 +203,33 @@ describe( 'config', function(){
     // -------------------------------------------
 
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            pluginOptions: {}
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          pluginOptions: {}
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
   });
 
   it( '对配置进行解析时, 若配置不合法, 将会退出打包程序 ( plugins )', async () => {
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            plugins: []
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          plugins: []
-        }, true);
+        expect( stdout ).is.includes('选项必须为一个函数并且函数返回 plugins 数组, 请检查您的配置文件');
       });
 
-      expect( stdout ).is.includes('选项必须为一个函数并且函数返回 plugins 数组, 请检查您的配置文件');
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
 
     // -------------------------------------------
@@ -264,35 +237,33 @@ describe( 'config', function(){
     // -------------------------------------------
 
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            plugins: () => []
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          plugins: () => []
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
   });
 
   it( '对配置进行解析时, 若入口文件不存在, 将会退出打包程序', async () => {
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            _code: null
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          _code: null
-        }, true);
+        expect( stdout ).is.includes('未找到需要打包的入口文件');
       });
 
-      expect( stdout ).is.includes('未找到需要打包的入口文件');
-      expect( processExit.isExit ).is.true;
-      processExit.end();
+      expect( isExit ).is.true;
     }
 
     // -------------------------------------------
@@ -300,18 +271,17 @@ describe( 'config', function(){
     // -------------------------------------------
 
     {
-      processExit.start();
-      expect( processExit.isExit ).is.false;
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            _code: 'null'
+          }, true);
+        });
 
-      const stdout = await proxyLog( async () => {
-        await compilerRollupConfigs({
-          _code: 'null'
-        }, true);
+        expect( stdout ).is.equals('');
       });
 
-      expect( stdout ).is.equals('');
-      expect( processExit.isExit ).is.false;
-      processExit.end();
+      expect( isExit ).is.false;
     }
   });
 
