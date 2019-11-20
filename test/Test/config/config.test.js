eeae29596f898dd511ee6430d73a800b849b7a3a
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-11-20 17:44:58 +0800

    调整单元测试顺序

--- test/Test/config/config.test.js
+++ test/Test/config/config.test.js
@@ -7,183 +7,6 @@ const proxyProcessExit = require('../../Lib/utils/proxyProcessExit');
 describe( 'config', function(){
   this.timeout( Infinity );
 
-  it( '未使用 pipe 选项进行多项目打包时, 当前配置表会视为一个有效的配置表', () => {
-    expect(
-      compilerRollupConfigs().length
-    ).is.equals( 1 );
-  });
-
-  it( '当使用 pipe 选项进行多项目打包时, 当前配置表将不再视为一个有效的配置表', () => {
-    const rollupConfigs = compilerRollupConfigs({
-      pipe: [
-        {},
-        {}
-      ]
-    });
-
-    expect( rollupConfigs.length ).is.equals( 2 );
-  });
-
-  it( '使用 configureRollup 可以用于修改已经解析完成的 rollup 配置', () => {
-    const rollupConfig = compilerRollupConfigs({
-      configureRollup: rollupConfig => {
-        rollupConfig.input.input = 'isEdit';
-      }
-    })[0];
-
-    expect( rollupConfig.input.input ).is.equals('isEdit');
-  });
-
-  it( '使用 configureRollup 将会始终采纳方法返回的 rollup 配置', () => {
-    const rollupConfig = compilerRollupConfigs({
-      configureRollup: rollupConfig => {
-        const newRollupConfig = Object.$assign( null, rollupConfig, {
-          input: {
-            input: 'isEdit'
-          }
-        });
-
-        expect( rollupConfig.input.input ).is.not.equals('isEdit');
-        expect( newRollupConfig.input.input ).is.equals('isEdit');
-
-        return newRollupConfig;
-      }
-    })[0];
-
-    expect( rollupConfig.input.input ).is.equals('isEdit');
-  });
-
-  it( '使用 configureRollup 选项时可以接受被解析后的 Hu-CLI 配置作为参数', () => {
-    let config;
-
-    compilerRollupConfigs({
-      xxx: 123,
-      configureRollup: ( rollupConfig, _config ) => config = _config
-    })[0];
-
-    expect( config.xxx ).is.equals( 123 );
-  });
-
-  it( '使用 mode 并指定为生产环境时, 会加载 @moomfe/hu-template-minifier 用于模板压缩', () => {
-    // 1
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: 'production'
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
-      ).is.not.undefined;
-    }
-    // 2
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: true
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
-      ).is.not.undefined;
-    }
-
-    // -------------------------------------------
-    // - 反向测试
-    // -------------------------------------------
-
-    // 1
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: 'development'
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
-      ).is.undefined;
-    }
-    // 2
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: false
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
-      ).is.undefined;
-    }
-  });
-
-  it( '使用 mode 并指定为生产环境时, 会加载 terser 用于代码压缩', () => {
-    // 1
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: 'production'
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'terser' })
-      ).is.not.undefined;
-    }
-    // 2
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: true
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'terser' })
-      ).is.not.undefined;
-    }
-
-    // -------------------------------------------
-    // - 反向测试
-    // -------------------------------------------
-
-    // 1
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: 'development'
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'terser' })
-      ).is.undefined;
-    }
-    // 2
-    {
-      const rollupConfig = compilerRollupConfigs({
-        mode: false
-      })[0];
-  
-      expect(
-        rollupConfig.input.plugins.$find({ name: 'terser' })
-      ).is.undefined;
-    }
-  });
-
-  it( '使用 plugins 选项可以安装自定义 rollup 插件', () => {
-    let code;
-
-    return runBuild({
-      _code: 'console.log(123)',
-      plugins: () => [
-        { transform: _code => code = _code }
-      ]
-    }).then(({ codes, logs }) => {
-      expect( code ).is.equals('console.log(123)');
-    });
-  });
-
-  it( '使用 plugins 选项时可以接受被解析后的 Hu-CLI 配置作为参数', () => {
-    let config;
-
-    compilerRollupConfigs({
-      xxx: 123,
-      plugins: _config => config = _config
-    })[0];
-
-    expect( config.xxx ).is.equals( 123 );
-  });
-
   it( '对配置进行解析时, 若配置合法, 打包程序会正确执行', async () => {
     const isExit = await proxyProcessExit( async () => {
       const stdout = await proxyLog( async () => {
@@ -584,6 +407,183 @@ describe( 'config', function(){
 
       expect( isExit ).is.false;
     }
+  });
+
+  it( '未使用 pipe 选项进行多项目打包时, 当前配置表会视为一个有效的配置表', () => {
+    expect(
+      compilerRollupConfigs().length
+    ).is.equals( 1 );
+  });
+
+  it( '当使用 pipe 选项进行多项目打包时, 当前配置表将不再视为一个有效的配置表', () => {
+    const rollupConfigs = compilerRollupConfigs({
+      pipe: [
+        {},
+        {}
+      ]
+    });
+
+    expect( rollupConfigs.length ).is.equals( 2 );
+  });
+
+  it( '使用 configureRollup 可以用于修改已经解析完成的 rollup 配置', () => {
+    const rollupConfig = compilerRollupConfigs({
+      configureRollup: rollupConfig => {
+        rollupConfig.input.input = 'isEdit';
+      }
+    })[0];
+
+    expect( rollupConfig.input.input ).is.equals('isEdit');
+  });
+
+  it( '使用 configureRollup 将会始终采纳方法返回的 rollup 配置', () => {
+    const rollupConfig = compilerRollupConfigs({
+      configureRollup: rollupConfig => {
+        const newRollupConfig = Object.$assign( null, rollupConfig, {
+          input: {
+            input: 'isEdit'
+          }
+        });
+
+        expect( rollupConfig.input.input ).is.not.equals('isEdit');
+        expect( newRollupConfig.input.input ).is.equals('isEdit');
+
+        return newRollupConfig;
+      }
+    })[0];
+
+    expect( rollupConfig.input.input ).is.equals('isEdit');
+  });
+
+  it( '使用 configureRollup 选项时可以接受被解析后的 Hu-CLI 配置作为参数', () => {
+    let config;
+
+    compilerRollupConfigs({
+      xxx: 123,
+      configureRollup: ( rollupConfig, _config ) => config = _config
+    })[0];
+
+    expect( config.xxx ).is.equals( 123 );
+  });
+
+  it( '使用 mode 并指定为生产环境时, 会加载 @moomfe/hu-template-minifier 用于模板压缩', () => {
+    // 1
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: 'production'
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
+      ).is.not.undefined;
+    }
+    // 2
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: true
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
+      ).is.not.undefined;
+    }
+
+    // -------------------------------------------
+    // - 反向测试
+    // -------------------------------------------
+
+    // 1
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: 'development'
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
+      ).is.undefined;
+    }
+    // 2
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: false
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'hu-template-minifier' })
+      ).is.undefined;
+    }
+  });
+
+  it( '使用 mode 并指定为生产环境时, 会加载 terser 用于代码压缩', () => {
+    // 1
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: 'production'
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'terser' })
+      ).is.not.undefined;
+    }
+    // 2
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: true
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'terser' })
+      ).is.not.undefined;
+    }
+
+    // -------------------------------------------
+    // - 反向测试
+    // -------------------------------------------
+
+    // 1
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: 'development'
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'terser' })
+      ).is.undefined;
+    }
+    // 2
+    {
+      const rollupConfig = compilerRollupConfigs({
+        mode: false
+      })[0];
+  
+      expect(
+        rollupConfig.input.plugins.$find({ name: 'terser' })
+      ).is.undefined;
+    }
+  });
+
+  it( '使用 plugins 选项可以安装自定义 rollup 插件', () => {
+    let code;
+
+    return runBuild({
+      _code: 'console.log(123)',
+      plugins: () => [
+        { transform: _code => code = _code }
+      ]
+    }).then(({ codes, logs }) => {
+      expect( code ).is.equals('console.log(123)');
+    });
+  });
+
+  it( '使用 plugins 选项时可以接受被解析后的 Hu-CLI 配置作为参数', () => {
+    let config;
+
+    compilerRollupConfigs({
+      xxx: 123,
+      plugins: _config => config = _config
+    })[0];
+
+    expect( config.xxx ).is.equals( 123 );
   });
 
 });
