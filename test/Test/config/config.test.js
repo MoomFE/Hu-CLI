728319e652d23597e8b9fe053091fec181098847
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-11-25 17:19:54 +0800

    完善单元测试

--- test/Test/config/config.test.js
+++ test/Test/config/config.test.js
@@ -324,7 +324,7 @@ describe( 'config', function(){
           }, true);
         });
 
-        expect( stdout ).is.includes('');
+        expect( stdout ).is.equals('');
       });
 
       expect( isExit ).is.false;
@@ -338,7 +338,87 @@ describe( 'config', function(){
           }, true);
         });
 
-        expect( stdout ).is.includes('');
+        expect( stdout ).is.equals('');
+      });
+
+      expect( isExit ).is.false;
+    }
+  });
+
+  it( '对配置进行解析时, 若配置不合法, 将会退出打包程序 ( externals ) ( 二 )', async () => {
+    // 1
+    {
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'iife',
+            externals: {
+              '@moomfe/hu': ''
+            }
+          }, true);
+        });
+
+        expect( stdout ).is.includes('下取值不正确, 请检查您的配置文件');
+      });
+
+      expect( isExit ).is.true;
+    }
+    // 2
+    {
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'iife',
+            externals: {
+              '@moomfe/hu': {
+                'iife': ''
+              }
+            }
+          }, true);
+        });
+
+        expect( stdout ).is.includes('下取值不正确, 请检查您的配置文件');
+      });
+
+      expect( isExit ).is.true;
+    }
+
+    // -------------------------------------------
+    // - 反向测试
+    // -------------------------------------------
+
+    // 1
+    {
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'iife',
+            externals: {
+              '@moomfe/hu': 'Hu'
+            }
+          }, true);
+        });
+
+        expect( stdout ).is.equals('');
+      });
+
+      expect( isExit ).is.false;
+    }
+    // 1
+    {
+      const isExit = await proxyProcessExit( async () => {
+        const stdout = await proxyLog( async () => {
+          await compilerRollupConfigs({
+            format: 'iife',
+            externals: {
+              '@moomfe/hu': {
+                iife: 'Hu'
+              }
+            }
+          }, true);
+        });
+
+        expect( stdout ).is.equals('');
       });
 
       expect( isExit ).is.false;
@@ -1162,6 +1242,209 @@ describe( 'config', function(){
         expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs', '@moomfe/hu' ]);
         expect( rollupConfig.output.globals ).is.deep.equals({
           '@moomfe/hu': 'Hu',
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+  });
+
+  it( '使用 externals 选项可以定义外部依赖, 值可以传入 JSON 以适配不同打包场景的外部依赖来源, 当前打包场景下未找到对应外部依赖时会忽略', () => {
+    const externals = {
+      '@moomfe/hu': {
+        'cjs': '@moomfe/hu',
+        'esm': '@moomfe/hu',
+        'esm.browser': 'https://cdn.jsdelivr.net/npm/@moomfe/hu'
+      }
+    };
+    const externalsMore = {
+      '@moomfe/zenjs': 'ZenJS',
+      '@moomfe/hu': {
+        'cjs': '@moomfe/hu',
+        'esm': '@moomfe/hu',
+        'esm.browser': 'https://cdn.jsdelivr.net/npm/@moomfe/hu'
+      }
+    };
+
+    // amd
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'amd',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([]);
+        expect( rollupConfig.output.globals ).is.deep.equals({});
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'amd',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+    // cjs
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'cjs',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/hu' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/hu': '@moomfe/hu'
+        });
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'cjs',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs', '@moomfe/hu' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/hu': '@moomfe/hu',
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+    // system
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'system',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([]);
+        expect( rollupConfig.output.globals ).is.deep.equals({});
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'system',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+    // esm
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'esm',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/hu' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/hu': '@moomfe/hu'
+        });
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'esm',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs', '@moomfe/hu' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/hu': '@moomfe/hu',
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+    // esm.browser
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'esm.browser',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/hu' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/hu': 'https://cdn.jsdelivr.net/npm/@moomfe/hu'
+        });
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'esm.browser',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs', '@moomfe/hu' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/hu': 'https://cdn.jsdelivr.net/npm/@moomfe/hu',
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+    // iife
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'iife',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([]);
+        expect( rollupConfig.output.globals ).is.deep.equals({});
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'iife',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
+          '@moomfe/zenjs': 'ZenJS'
+        });
+      }
+    }
+    // umd
+    {
+      // 单个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'amd',
+          externals
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([]);
+        expect( rollupConfig.output.globals ).is.deep.equals({});
+      }
+      // 多个外部依赖
+      {
+        const rollupConfig = compilerRollupConfigs({
+          format: 'amd',
+          externals: externalsMore
+        })[0];
+
+        expect( rollupConfig.input.external ).is.deep.equals([ '@moomfe/zenjs' ]);
+        expect( rollupConfig.output.globals ).is.deep.equals({
           '@moomfe/zenjs': 'ZenJS'
         });
       }
