ff2fe18f5d4b2c592eba7ea1d5190726d750d37c
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-04 17:08:31 +0800

    分离出 externals 选项的单元测试

--- test/Test/index.js
+++ test/Test/index.js
@@ -8,7 +8,8 @@ process.cwd = () => {
 
 
 require('./config/config.test');
+require('./config/config.externals.test');
 require('./command/build.test');
 require('./plugins/banner.test');
-require('./plugins/replace.test');
 require('./plugins/json.test');
+require('./plugins/replace.test');
