7d8abb360ac2d27dbf3f65e6d9bce6b8c1d68408
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-09 11:10:14 +0800

    执行单元测试前先检测单元测试环境所需环境并安装

---
+++ test/Test/install.js
@@ -0,0 +1,14 @@
+const { promisify } = require('util');
+const { exec } = require('child_process');
+const { root } = require('../Lib/const');
+
+
+!async function(){
+  try{
+    require('../node_modules/@moomfe/hu/package.json');
+  }catch( error ){
+    return promisify( exec )(`npm install @moomfe/hu`, {
+      cwd: root
+    });
+  }
+}();
