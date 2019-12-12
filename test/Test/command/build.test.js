5e9c2f8d9e8714b3e3ea39ed72ee9e4bc5c53b92
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-12-12 14:44:15 +0800

    完善单元测试

--- test/Test/command/build.test.js
+++ test/Test/command/build.test.js
@@ -1,9 +1,58 @@
 require('@moomfe/zenjs');
+const fs = require('fs-extra');
+const path = require('path');
 const expect = require('chai').expect;
 const compilerRollupConfigs = require('../../Lib/compilerRollupConfigs');
+const runBuild = require('../../Lib/runBuild');
+const runBuildCommand = require('../../Lib/runBuildCommand');
+const proxyLog = require('../../Lib/utils/proxyLog');
+const proxyProcessExit = require('../../Lib/utils/proxyProcessExit');
+const { root } = require('../../Lib/const');
 
 
 describe( 'command.build', function(){
   this.timeout( Infinity );
+
+  it( '默认方式使用 build 指令', async () => {
+    let isRun = false;
+    const isExit = await proxyProcessExit( async () => {
+      await runBuildCommand({
+        _code: `console.log(123)`
+      }).then(({ codes: [ code ], logs }) => {
+        isRun = true;
+        expect( code ).is.includes(`console.log(123)`);
+      });
+    });
+
+    expect( isRun ).is.true;
+    expect( isExit ).is.false;
+  });
+
+  it( '使用 build 指令时带上 -c, --config 参数', async () => {
+    let isRun = false;
+    const isExit = await proxyProcessExit( async () => {
+      const configPath = path.resolve( root, 'other.config.js' );
+      const code = `module.exports = {
+        replace: {
+          'console.log(123)': 'alert(456)'
+        }
+      }`;
+
+      await fs.outputFile( configPath, code )
+      await runBuildCommand(
+        {
+          _code: `console.log(123)`
+        },
+        'bin\\hu build -c other.config.js'
+      ).then(({ codes: [ code ], logs }) => {
+        isRun = true;
+        expect( code ).is.not.includes(`console.log(123)`);
+        expect( code ).is.includes(`alert(456)`);
+      });
+    });
+
+    expect( isRun ).is.true;
+    expect( isExit ).is.false;
+  });
 
 });
