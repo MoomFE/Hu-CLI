3cbc111fa2076c1b4c256ee2181b17d7bbbc25fc
wei.zhang <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-03-26 18:18:01 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,11 +35,16 @@ module.exports = {
     "no-param-reassign": "off",
     // 强制行的最大长度
     "max-len": [ "error", 180, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
+      // 忽略含有链接的行
+      "ignoreUrls": true,
+      // 忽略所有拖尾注释和行内注释
+      "ignoreComments": false,
+      // 忽略包含正则表达式的行
+      "ignoreRegExpLiterals": true,
+      // 忽略含有双引号或单引号字符串的行
+      "ignoreStrings": true,
+      // 忽略包含模板字面量的行
+      "ignoreTemplateLiterals": true,
     }],
     // 禁止未使用过的表达式
     "no-unused-expressions": [ "error", {
